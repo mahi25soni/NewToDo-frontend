5a36faedc47769fefeffed9225dafa22276667d5
mahi25soni <msdraj02@protonmail.com>
2024-04-27 15:20:35 +0530

    Login done

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,16 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import './App.css'
 import Navbar from './components/Navbar/Navbar'
 import Signup from './components/Auth/Signup'
+import Login from './components/Auth/Login'
 
 function App() {
 
   return (
     <BrowserRouter>
     <Navbar></Navbar>
-    <Signup></Signup>
     <Routes>
+      <Route path='/signup' element={<Signup />} />
+        <Route path='/login' element={<Login />} />
 
     </Routes>
     </BrowserRouter>
--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,7 +1,31 @@
 import React from 'react'
+import './Auth.css'
+import lendpage from '../../assets/photu.png'
 
 export default function Login() {
   return (
-    <div>Login</div>
+    <div className='main-box'>
+        <div>
+            <img src={lendpage} alt="" id="lend_image" />
+        </div>
+
+        <div>
+            <form action="" style={{
+                display : "flex",
+                flexDirection : "column",
+                alignItems : "center"
+            }}>
+                <input type="text" name="email" id="" placeholder='Enter your email'/>
+
+                <input type="text" name="password" id="" placeholder='Enter your password'/>
+
+                <button style={{
+                    marginTop : "1rem"
+                }}>Login In</button>
+            </form>
+        </div>
+
+
+    </div>
   )
 }
